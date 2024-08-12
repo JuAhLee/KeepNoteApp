8f47e7c2c107dab773e964c62d6d2af1b1315d3f
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-08-13 01:09:08 +0900

    Notes-Page PinnedArray

--- src/utils/getAllNotes.tsx
+++ src/utils/getAllNotes.tsx
@@ -3,16 +3,65 @@ import { NotesContainer } from "../styles/styles";
 import { Note } from "../types/note";
 
 const getAllNotes = (mainNotes: Note[], filter: string) => {
-  return (
-    <>
-      <div className="allNotes__notes-type">All Notes</div>
-      <NotesContainer>
-        {mainNotes.map((note) => (
-          <NoteCard key={note.id} note={note} type="notes" />
-        ))}
-      </NotesContainer>
-    </>
-  );
+  const pinned = mainNotes.filter(({ isPinned }) => isPinned);
+  const normal = mainNotes.filter(({ isPinned }) => !isPinned);
+
+  if (normal.length !== 0 && pinned.length === 0) {
+    return (
+      <>
+        <div className="allNotes__notes-type">
+          All Notes<span>({normal.length})</span>
+        </div>
+        <NotesContainer>
+          {normal.map((note) => (
+            <NoteCard key={note.id} note={note} type="notes" />
+          ))}
+        </NotesContainer>
+      </>
+    );
+  }
+
+  if (pinned.length !== 0 && normal.length === 0) {
+    return (
+      <>
+        <div className="allNotes__notes-type">
+          Pinned Notes<span>({pinned.length})</span>
+        </div>
+        <NotesContainer>
+          {pinned.map((note) => (
+            <NoteCard key={note.id} note={note} type="notes" />
+          ))}
+        </NotesContainer>
+      </>
+    );
+  }
+
+  if (pinned.length !== 0 && normal.length !== 0) {
+    return (
+      <>
+        <div>
+          <div className="allNotes__notes-type">
+            Pinned Notes<span>({pinned.length})</span>
+          </div>
+          <NotesContainer>
+            {pinned.map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+        <div>
+          <div className="allNotes__notes-type">
+            All Notes<span>({normal.length})</span>
+          </div>
+          <NotesContainer>
+            {normal.map((note) => (
+              <NoteCard key={note.id} note={note} type="notes" />
+            ))}
+          </NotesContainer>
+        </div>
+      </>
+    );
+  }
 };
 
 export default getAllNotes;
